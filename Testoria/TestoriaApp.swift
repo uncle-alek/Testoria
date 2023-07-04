ef130b37d29ce9c87fd41434317d0ea504058d11
Aleksey Yakimenko <aleksey.yakimenko@agoda.com>
2023-07-04 15:43:00 +0700

    Add skeleton

---
+++ Testoria/TestoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestoriaApp.swift
+//  Testoria
+//
+//  Created by Aleksey Yakimenko on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TestoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
